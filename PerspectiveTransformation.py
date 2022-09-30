6146b4ec245170207e737e13f4e2d6256722ce67
MrTGBruh <114698790+MrTGBruh@users.noreply.github.com>
2022-09-30 14:20:50 +0500

    Update PerspectiveTransformation.py

--- PerspectiveTransformation.py
+++ PerspectiveTransformation.py
@@ -12,10 +12,10 @@ class PerspectiveTransformation:
     """
     def __init__(self):
         """Init PerspectiveTransformation."""
-        self.src = np.float32([(430, 350),     # top-left
+        self.src = np.float32([(430, 450),     # top-left
                                (150, 720),     # bottom-left
                                (1200, 720),    # bottom-right
-                               (880, 350)])    # top-right
+                               (880, 450)])    # top-right
         self.dst = np.float32([(100, 0),
                                (100, 720),
                                (1100, 720),
